217f18a0733b0e32f106aa6be38720f7ce36bc81
gaurav-kumar22 <gorav.panwar99@gmail.com>
2024-06-07 07:41:46 +0530

    add all file and folders

---
+++ app.js
@@ -0,0 +1,110 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
+const multer = require('multer');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI = (`mongodb+srv://choudharygaurav884:310886Cluster.@@node.gzezwi7.mongodb.net`)
+
+const app = express();
+const store = new MongoDBStore({
+
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const csrfProtection = csrf();
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+// const { Console } = require('console');
+// const { request } = require('https');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(multer({ storage: fileStorage }).single('image'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: true,
+    saveUninitialized: true,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  console.log(req.csrfToken)
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    console.log("in if", req.session)
+    return next();
+
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        console.log("that is!user")
+        return next();
+      }
+      req.user = user;
+      console.log("thhat is user")
+      next();
+    })
+    .catch(err => {
+      next(new Error(err));
+    });
+});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.get('/500', errorController.get500);
+
+app.use(errorController.get404);
+
+app.use((req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  console.log("this is a session", req.session);
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(result => {
+    app.listen(3000);
+    console.log("server is Running");
+  })
+  .catch(err => {
+    console.log("this is error", err);
+  });
